1fa36c776ee84de046aec71dff71f99928db23a8
avadex <85082306+avadex@users.noreply.github.com>
2022-03-18 23:51:08 +0100

    Update contracts.js

--- constants/contracts.js
+++ constants/contracts.js
@@ -6,7 +6,7 @@ module.exports.FACTORY_ABI = abis.factoryABI
 module.exports.ROUTER_ADDRESS = '0x6A8f98d7e34Fd214B428BFc68c9309Ea3C4Fc7F1'
 module.exports.ROUTER_ABI = abis.routerABI
 
-module.exports.GAUGES_ADDRESS = '0x3408422f63535EAb2eF5B199edecd0c6189Cc03a'
+module.exports.GAUGES_ADDRESS = '0x47a3931486a73314D367e4dE2B2FfaC83fa4B81E'
 module.exports.GAUGES_ABI = abis.gaugesABI
 
 module.exports.ERC20_ABI = abis.erc20ABI
