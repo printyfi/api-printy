636eb4cd5547808d79ea3fe84843fed7db65d2ee
printyfi <102478248+printyfi@users.noreply.github.com>
2024-01-19 19:17:00 +0100

    Update callUpdates.js

--- callUpdates.js
+++ callUpdates.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const request = require('request-promise')
 
  //const url = process.env.API_URL
-const url = 'https://api.printy.fi'
+const url = 'https://api.printyfinance.xyz'
 //const url = 'http://localhost'
 
 async function doWork() {
